4349a77a783a30a3943a7a31f9d0c02a4f928a28
David Kamphausen <david.kamphausen@web.de>
2012-08-06 21:09:29 +0200

    working highscore service

--- app.rb
+++ app.rb
@@ -3,37 +3,30 @@ require 'pp'
 require 'json'
 require 'data_mapper'
 
-DataMapper.setup(:default, 'sqlite::memory:')
+DataMapper.setup(:default, 'sqlite://'+File.expand_path('../highscore.db',__FILE__))
 
 class HighscoreList
   include DataMapper::Resource
-  property :id,         Serial  
+  property :id, Serial  
   property :data,String
 end
 
-
-DataMapper.auto_migrate!
+DataMapper.auto_upgrade!
 
 def getConfig
   HighscoreList.first||HighscoreList.create(:data=>[].to_json)
 end
 
-
 get '/save' do
-  pp params
-  list=JSON.parse(getConfig.data)
-  pp "READ:",list,getConfig
+  config=getConfig
+  list=JSON.parse(config.data)
   list<<{"value"=>params["score"].to_i,"name"=>params["name"]}
-  pp list
   list=list.sort {|a,b|
-    pp "A;",a,b
-    a["value"]<=>b["value"]}[0..9]
+    a["value"]<=>b["value"]}.reverse[0..9]
   headers 'Content-Type'=>'application/json'
-  c=getConfig
-  c.data=list.to_json
-  pp "DATA",c
-  c.save
-  list.to_json
+  json=list.to_json
+  config.update!(:data=>json)
+  json
 end
 get '/' do
   headers 'Content-Type'=>'application/json'
